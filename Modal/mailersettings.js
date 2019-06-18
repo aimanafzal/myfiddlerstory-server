c6230a359a2d476ead6465c9c69499a515231ffe
aimanafzal <ayman.afzal@msn.com>
2019-06-18 16:10:36 +0500

    Add files via upload

--- Modal/mailersettings.js
+++ Modal/mailersettings.js
@@ -1,13 +1,13 @@
 module.exports = {
     service: 'gmail',
     auth: {
-        user: '',
-        pass: '',
+        user: 'myfiddlerstory@gmail.com',
+        pass: '81MRTK@!',
         port: 587,
         authentication: 'plain'
     },
     mailOptions: {
-        from: '',
+        from: 'myfiddlerstory@gmail.com',
         subject: 'Thankyou for being with #MYFIDDLESTORY',
         text: 'Thankyou for being with #MYFIDDLESTORY'
     }
