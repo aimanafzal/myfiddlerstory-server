806e0be6531aeef3c49f357a237ce139d65d916f
aimanafzal <ayman.afzal@msn.com>
2019-06-18 20:44:33 +0500

    Add files via upload

---
+++ Modal/upload_s3_bucket.js
@@ -0,0 +1,38 @@
+
+const config = require('../config').config;
+let nodemailer = require('nodemailer');
+let mailerSettings = require('./mailersettings')
+const collectionName = require('./collectionName');
+let aws = require('aws-sdk');
+let multer = require('multer');
+let multerS3 = require('multer-s3');
+
+
+function upload_s3_bucket() { }
+upload_s3_bucket.prototype.upload = function (file, res) {
+    let s3bucket = new aws.S3({
+        secretAccessKey: '7BIL9JDzIjuJ3kfalaMm8kXxnoq+08eqPGNoMuej',
+        accessKeyId: 'AKIA4Q5EFT7XWPY2RPEC',
+        region: 'us-east-2'
+    })
+    var params = {
+        Bucket: 'myfiddlerstory',
+        Key: file.name,
+        Body: file.data
+    }
+    s3bucket.upload(params, function (err, data) {
+        if (err) {
+            // console.log('error in callback')
+            // console.log(err)
+            res.send({ err: err })
+        }
+        else {
+            // console.log('success')
+            // console.log(data)
+            res.send({ data: data })
+        }
+    })
+}
+
+
+module.exports = upload_s3_bucket;
