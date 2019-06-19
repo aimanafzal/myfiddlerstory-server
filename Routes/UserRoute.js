438548e8696d92475fee6ea5e33288740fa69b92
aimanafzal <ayman.afzal@msn.com>
2019-06-19 08:44:45 +0500

    Add files via upload

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -2,6 +2,12 @@ const express = require('express');
 const UserRouter = express.Router();
 let survey_information = require('../Modal/Survey_Information');
 let Survey_Information_fetch = require('../Modal/Survey_Information_fetch');
+let SecondGeneration_Survey_Information = require('../Modal/Second_Generation_Survey_Information');
+let SecondGeneration_Survey_Information_fetch = require('../Modal/Second_Generation_Survey_Information_fetch');
+let FirstGeneration_DocumentID = require('../Modal/First_Generation_DocumentID_Fetch');
+let SecondGeneration_DocumentID = require('../Modal/Second_Generation_DocumentID_Fetch')
+let FirstGeneration_Survey_Information_ByDocumentID = require('../Modal/First_Generation_Survey_Information_BYDocumentID_fetch')
+let SecondGeneration_Survey_Information_ByDocumentID = require('../Modal/Second_Generation_Survey_Information_BYDocumentID_fetch')
 let mailer = require('../Modal/mailer')
 let upload = require('../Modal/upload')
 let uploads = require('../Modal/upload_s3_bucket')
@@ -12,6 +18,14 @@ UserRouter
         let obj = new Survey_Information_fetch()
         obj.fetch_record(res);
     });
+UserRouter
+    .route('/fetch_second_generation_survey_information')
+    .post(function (req, res) {
+        let obj = new SecondGeneration_Survey_Information_fetch()
+        obj.fetch_record(res);
+    });
+
+
 UserRouter
     .route('/add_survey_information')
     .post(function (req, res) {
@@ -62,6 +76,89 @@ UserRouter
 
     });
 
+
+UserRouter.route('/add_second_generation_survey_information').post(function (req, res) {
+    let FullName = (req.body.FullName !== '') ? req.body.FullName : null;
+    let EmailAddress = (req.body.EmailAddress !== '') ? req.body.EmailAddress : null;
+    let Anonymous = req.body.Anonymous;
+    let question1_blank1 = req.body.question1_blank1;
+    let question2_blank1 = req.body.question2_blank1;
+    let question3_blank1 = req.body.question3_blank1;
+
+    let question4_blank1 = req.body.question4_blank1;
+    let question4_blank2 = req.body.question4_blank2;
+    let question4_blank3 = req.body.question4_blank3;
+    let question4_blank4 = req.body.question4_blank4;
+    let question4_blank5 = req.body.question4_blank5;
+
+    let question5_blank1 = req.body.question5_blank1;
+
+    let question6_blank1 = req.body.question6_blank1;
+    let question6_blank2 = req.body.question6_blank2;
+    let question6_blank3 = req.body.question6_blank3;
+    let question6_blank4 = req.body.question6_blank4;
+
+    let question7_blank1 = req.body.question7_blank1;
+    let question8_blank1 = req.body.question8_blank1;
+    let question9_blank1 = req.body.question9_blank1;
+    let question10_blank1 = req.body.question10_blank1;
+    let question11_blank1 = req.body.question11_blank1;
+    let question12_blank1 = req.body.question12_blank1;
+    let question13_blank1 = req.body.question13_blank1;
+    let question14_blank1 = req.body.question14_blank1;
+    let question15_blank1 = req.body.question15_blank1;
+    let question16_blank1 = req.body.question16_blank1;
+    let question17_blank1 = req.body.question17_blank1;
+
+
+    let obj = new SecondGeneration_Survey_Information(FullName,
+        EmailAddress,
+        Anonymous,
+        question1_blank1,
+        question2_blank1,
+        question3_blank1,
+        question4_blank1, question4_blank2, question4_blank3, question4_blank4, question4_blank5,
+        question5_blank1,
+        question6_blank1, question6_blank2, question6_blank3, question6_blank4,
+        question7_blank1,
+        question8_blank1,
+        question9_blank1,
+        question10_blank1,
+        question11_blank1,
+        question12_blank1,
+        question13_blank1,
+        question14_blank1,
+        question15_blank1,
+        question16_blank1,
+        question17_blank1);
+    obj.addRecord(res);
+
+});
+
+
+
+UserRouter.route('/fetch_first_generation_documentid').post(function (req, res) {
+     let obj = new FirstGeneration_DocumentID();
+     obj.fetch_record(res);
+});
+
+UserRouter.route('/fetch_second_generation_documentid').post(function (req, res) {
+    let obj = new SecondGeneration_DocumentID();
+    obj.fetch_record(res);
+});
+
+
+UserRouter.route('/FirstGeneration_Survey_Information_ByDocumentID').post(function (req, res) {
+    let obj = new FirstGeneration_Survey_Information_ByDocumentID();
+    let documentID = req.body.documentID;
+    obj.fetch_record(res, documentID);
+});
+
+UserRouter.route('/SecondGeneration_Survey_Information_ByDocumentID').post(function (req, res) {
+    let obj = new SecondGeneration_Survey_Information_ByDocumentID();
+    let documentID = req.body.documentID;
+    obj.fetch_record(res, documentID);
+});
 /**
  * usage:
  * {
