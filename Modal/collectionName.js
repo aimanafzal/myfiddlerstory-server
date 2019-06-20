aade96f7b29ad55a715303aeb31dc430dae18963
aimanafzal <ayman.afzal@msn.com>
2019-06-20 22:29:18 +0500

    Add files via upload

--- Modal/collectionName.js
+++ Modal/collectionName.js
@@ -1,3 +1,4 @@
 module.exports = {
-    Survey_Information: "Survey_Information"
+    Survey_Information: "Survey_Information",
+    Second_Generation_Survey_Information: "Second_Generation_Survey_Information"
 }
